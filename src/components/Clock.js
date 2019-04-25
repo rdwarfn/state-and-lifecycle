8cbf37a4a99bf3a650a18020db894d108e9671e3
mint <r.arifin@gmail.com>
2019-04-26 02:30:48 +0700

    add comments

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -7,22 +7,6 @@ class Clock extends Component {
             date: new Date()
         };
     }
-    // Wrong
-    // this.state.comment = 'Hello';
-
-    // Correct
-    // this.setState({comment: 'Hello'});
-
-    // For example, this code may fail to update the counter:
-    // Wrong
-    // this.setState({
-    // counter: this.state.counter + this.props.increment,
-    // });
-
-    // Correct
-    // this.setState((state, props) => ({
-    // counter: state.counter + props.increment
-    // }));
     componentDidMount() {
         this.timerID = setInterval(
             () => this.tick(),
@@ -52,3 +36,46 @@ class Clock extends Component {
 }
 
 export default Clock;
+
+// Wrong
+// this.state.comment = 'Hello';
+
+// Correct
+// this.setState({comment: 'Hello'});
+
+// For example, this code may fail to update the counter:
+// Wrong
+// this.setState({
+// counter: this.state.counter + this.props.increment,
+// });
+
+// Correct
+// this.setState((state, props) => ({
+// counter: state.counter + props.increment
+// }));
+
+
+// When you call setState(), React merges the object you provide into the current state.
+// For example, your state may contain several independent variables:
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       posts: [],
+//       comments: []
+//     };
+//   }
+
+// Then you can update them independently with separate setState() calls:
+//   componentDidMount() {
+//     fetchPosts().then(response => {
+//       this.setState({
+//         posts: response.posts
+//       });
+//     });
+
+//     fetchComments().then(response => {
+//       this.setState({
+//         comments: response.comments
+//       });
+//     });
+//   }
